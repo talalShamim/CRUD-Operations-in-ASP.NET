0e9f5e58fe9c322c5067661c2508292d72bc76ec
talal <talalshamim43@gmail.com>
2025-01-08 03:02:30 +0500

    Initial commit

---
+++ Data/ApplicationDBcontext.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models.Entities;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDBcontext : DbContext
+    {
+        public ApplicationDBcontext(DbContextOptions<ApplicationDBcontext> options) : base(options)
+        {
+            
+        }
+
+
+        public DbSet<Student> Students{ get; set; }
+    }
+}
